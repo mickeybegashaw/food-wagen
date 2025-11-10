e5f4ac6b408589fc87e95dd4b76e44b46e4e0597
Mikiyas Begashaw <mickeyjob2004@gmail.com>
2025-11-10 12:18:27 +0300

    update styles and structure for Header component;

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,22 @@
 import Image from "next/image";
-
+import Logo from "@/public/Images/logo.png"
 const Header = () => {
   return (
     <header
-      className="food-header
- 
-"
+      className="food-header"
     >
-      <div className="flex items-center gap-[11.5px] cursor-pointer">
+      <div className="flex items-center gap-[5.5px] cursor-pointer">
         <Image
-          src="/logo.png"
+          src={Logo}
           alt="Food Wagen Logo"
-          width={28}
-          height={29.98}
+          width={20}
+          height={21.98}
         />
-        <h1 className="text-3xl font-bold txt-primary">
-          {" "}
-          Food <span className="txt-secondary">Wagen</span>{" "}
+        <h1 className="text-[21px] md:text-[23px] font-bold txt-primary">
+          Food<span className="txt-secondary">Wagen</span>
         </h1>
       </div>
-      <button className="btn-secondary">Add Meal</button>
+      <button className="btn-secondary py-1.5  px-3 md:px-7 rounded-lg text-[15px] font-bold shadow-[0px_20px_40px_0px_#FFAE004A]">Add Meal</button>
     </header>
   );
 };
